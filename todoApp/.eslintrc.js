a24be0fab15220bb8e168a034d96e8ce01d278ab
formateur357 <formateur357@gmail.com>
2024-12-09 09:54:03 +0100

    first commit

---
+++ todoApp/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["react", "@typescript-eslint"],
+  env: {
+    browser: true,
+    es6: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    // Vos règles personnalisées
+  },
+};
