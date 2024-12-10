eaaacc3e829f1c32d0452b510dd2328b837fb05a
formateur357 <formateur357@gmail.com>
2024-12-10 10:31:36 +0100

    refacto

--- todoApp/src/App.tsx
+++ todoApp/src/App.tsx
@@ -1,7 +1,7 @@
 import TodoList from './components/TodoList';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import './App.css';
+import './assets/styles/App.css';
 
 function App() {
   console.log('rendu App');
